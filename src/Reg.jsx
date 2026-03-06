bc1a0b1406454e2cd0112d967e5457baa0d3e688
mounika045 <mounikakrishna361@gmail.com>
2026-03-06 11:05:18 +0530

    Update Reg.jsx

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -19,7 +19,7 @@ function Reg()
       {
         //  const res=await axios.post("http://localhost:8080/reg",data)
         const res = await axios.post(
-        "https://cseghrender-gx4q.onrender.com",
+        "https://cseghrender-gx4q.onrender.com/register",
         data
       )
          alert(res.data)
